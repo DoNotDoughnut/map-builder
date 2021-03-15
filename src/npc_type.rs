1534876e3d9a456361b5ff365afab743e7159224
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-03-14 21:29:59 -0700

    updated dependencies

--- src/npc_type.rs
+++ src/npc_type.rs
@@ -1,6 +1,6 @@
-use firecore_world::serialized::NPCType;
+use firecore_world::serialized::SerializedNPCType;
 
-pub fn load_npc_types(npc_type_dir: &str) -> crate::ResultT<Vec<NPCType>> {
+pub fn load_npc_types(npc_type_dir: &str) -> crate::ResultT<Vec<SerializedNPCType>> {
     let mut types = Vec::new();
 
     let dir = std::fs::read_dir(npc_type_dir)?;
@@ -9,12 +9,15 @@ pub fn load_npc_types(npc_type_dir: &str) -> crate::ResultT<Vec<NPCType>> {
             let path = entry.path();
             let name = entry.file_name().to_string_lossy().to_string();
             if path.is_dir() {
+                let data_path = path.join(name.clone() + ".ron");
                 let sprite_path = path.join(name.clone() + ".png");
                 let battle_sprite_path = path.join("battle.png");
                 let bytes =  std::fs::read(sprite_path)?;
+                let data = ron::from_str(&std::fs::read_to_string(data_path)?)?;
                 println!("Added NPC type {}!", &name);
-                let mut npc_type = NPCType {
-                    name,
+                let mut npc_type = SerializedNPCType {
+                    identifier: name,
+                    data,
                     sprite: bytes,
                     battle_sprite: None,
                 };
